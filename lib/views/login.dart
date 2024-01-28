d94bb580555b19d899baa4631447a4793e78a2b9
kato atsushi <ka.baseball1997@gmail.com>
2024-01-28 12:32:51 +0900

    add google login logic

--- lib/views/login.dart
+++ lib/views/login.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 // ログイン画面用Widget
 class LoginPage extends StatefulWidget {
@@ -13,6 +15,24 @@ class _LoginPageState extends State<LoginPage> {
   // 入力したメールアドレス・パスワード
   String email = '';
   String password = '';
+
+  Future<UserCredential> signInWithGoogle() async {
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication? googleAuth =
+        await googleUser?.authentication;
+
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    // Once signed in, return the UserCredential
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -71,6 +91,32 @@ class _LoginPageState extends State<LoginPage> {
                 ),
               ),
               const SizedBox(height: 8),
+              Container(
+                width: double.infinity,
+                // ログイン登録ボタン
+                child: OutlinedButton(
+                  child: Row(children: [
+                    Expanded(child: SizedBox()),
+                    Container(
+                      child: Image.asset('assets/google.png'),
+                      height: 40,
+                    ),
+                    Text('Google'),
+                    Expanded(child: SizedBox()),
+                  ]),
+                  onPressed: () async {
+                    try {
+                      await signInWithGoogle();
+                      await Navigator.pushReplacementNamed(context, '/map');
+                    } catch (e) {
+                      // ログインに失敗した場合
+                      setState(() {
+                        infoText = "ログインに失敗しました：${e.toString()}";
+                      });
+                    }
+                  },
+                ),
+              ),
               Container(
                 width: double.infinity,
                 // ログイン登録ボタン
